3fff3f303a27e9d784ae36839cf2940e4754a72b
Thiago Cesar <andrade-tc@uol.com.br>
2024-09-21 18:28:21 -0300

    Criando o projeto

--- js/app.js
+++ js/app.js
@@ -1,18 +1,64 @@
+let sorteioRealizado = false
 let listaAmigos = []
-nomeAmigo = document.getElementById('nome-amigo')
+let nomeAmigo = document.getElementById('nome-amigo')
+let amigosIncluidos = document.getElementById('lista-amigos')
+let listaSorteio = document.getElementById('lista-sorteio')
 
 function adicionar(){
     let amigo = nomeAmigo.value
+
+    if(amigo == ''){
+        alert('Por favor, insira um nome de uma pessoa para adicionar')
+        return
+    }
+
+    if(listaAmigos.includes(amigo)){
+        alert('Este amigo já foi adicionado')
+        return
+    }
+
     listaAmigos.push(amigo)
-    nomeAmigo.value = ''
-    let amigosIncluidos = document.getElementById('lista-amigos')
-    amigosIncluidos.textContent = listaAmigos
+    nomeAmigo.value = ''    
+    amigosIncluidos.textContent = listaAmigos.join(', ')
 }
 
 function sortear(){
+    if(sorteioRealizado){
+        alert('O sorteio já foi realizado, por favor, limpe a seleção antes de sortear novamente')
+        return
+    }
 
+    if(listaAmigos.length < 2){
+        alert('É necessário ao menos duas pessoas para realizar o sorteio do amigo oculto')
+        return
+    }    
+
+    let amigosParaSortear = [...listaAmigos]
+    amigosParaSortear.sort(() => Math.random() - 0.5)
+    let resultado = []
+
+    for (let i = 0; i < listaAmigos.length; i++){
+        if(listaAmigos[i] === amigosParaSortear[i]){
+            amigosParaSortear.sort(() => Math.random() - 0.5)
+            i = -1
+        }else{
+        resultado.push(`${listaAmigos[i]} vai presentear ${amigosParaSortear[i]}`)
+        }
+    }
+    
+    listaSorteio.innerHTML = resultado.join('<br>')
+    sorteioRealizado = true
 }
 
 function reiniciar(){
+    if(amigosIncluidos.textContent == '' && listaSorteio.innerHTML == '' && nomeAmigo.value == ''){
+        alert('Não existem dados para serem apagados')
+        return
+    }
 
+    listaAmigos = []    
+    sorteioRealizado = false
+    amigosIncluidos.textContent = ''
+    listaSorteio.innerHTML = ''
+    nomeAmigo.value = ''
 }
